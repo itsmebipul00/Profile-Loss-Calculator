bbb72a39d69dc59c2af738c8412dd037ad9ed184
bipul <itsmebipul00@gmail.com>
2021-10-01 15:58:16 +0530

    finl

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let costPrice = document.querySelector(".cost-price");
+let sellingPrice = document.querySelector(".selling-price");
+let submitBtn = document.querySelector(".btn");
+let output = document.querySelector(".output");
+
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  let cp = Number(costPrice.value);
+  let sp = Number(sellingPrice.value);
+
+  calculateProfitAndLoss(cp, sp);
+}
+
+
+function calculateProfitAndLoss(cp, sp) {
+  if (cp > sp) {
+    let loss = cp -sp ;
+    var lossPercent = ((loss / cp) * 100).toFixed(2);
+
+    showOutput(
+      `Loss is ${loss} and the percent is ${lossPercent}%`
+    );
+    output.classList.remove('profit', 'noProfitNorLoss')
+    output.classList.add('loss');
+  } else if (sp > cp) {
+    var profit =sp - cp;
+    var profitPercent = ((profit / cp) * 100).toFixed(2);
+
+    showOutput(
+      `The profit is ${profit} and the percent is ${profitPercent}%`
+    );
+    output.classList.remove('loss', 'noProfitNorLoss')
+    output.classList.add('profit')
+  } else {
+    showOutput(`Neither profit neither loss`);
+    output.classList.remove('loss', 'profit')
+    output.classList.add('noProfitNorLoss')
+  }
+}
+
+function showOutput(message) {
+  output.innerHTML = message;
+}
